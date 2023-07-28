e9ec81b0c4cf6a85d3ba03a76fb2af428a950f2e
Rothana69 <godkiller69@gmail.com>
2023-07-28 22:11:47 +0700

    fix error and some style

--- frontend/src/views/AdminVideo.vue
+++ frontend/src/views/AdminVideo.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="pa-8 fill-height">
+    <div class="pa-4 fill-height bg-blue-grey-darken-4">
         <h1>VIDEO MANAGEMENT</h1>
     </div>
 </template>
+<style scoped></style>
