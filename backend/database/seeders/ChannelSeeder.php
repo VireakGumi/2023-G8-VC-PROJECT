4d5c9e11616d7a27df9bd9627a5327feeba185cd
VireakGumi <chanvireak906@gmail.com>
2023-07-30 10:44:12 +0700

    fix image

--- backend/database/seeders/ChannelSeeder.php
+++ backend/database/seeders/ChannelSeeder.php
@@ -15,13 +15,13 @@ class ChannelSeeder extends Seeder
     {
         //
         $channels = [
-            ['name' => 'The best channel', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 1, 'profile' => '571963-star-wars-4k.jpg'],
-            ['name' => 'BoomRange', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 3, 'profile' => '19457.jpg'],
-            ['name' => 'Fantacy', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 4, 'profile' => '568018-fantasy-art.jpg'],
-            ['name' => 'kaka', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 5, 'profile' => '568018-fantasy-art.jpg']
+            ['name' => 'The best channel', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 1, 'profile' => 'litle_boy_and_litle_girl.JPG'],
+            ['name' => 'BoomRange', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 3, 'profile' => 'boy_and_dog.JPG'],
+            ['name' => 'Fantacy', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 4, 'profile' => 'cute_girl_and_tiger.JPG'],
+            ['name' => 'kaka', 'description' => 'best of the world', 'date_time' => '2023/7/5 11:36:14', 'user_id' => 5, 'profile' => 'purple_hair_child.JPG']
         ];
         foreach ($channels as $channel) {
-            Channel::create($channel);
+            Channel::create($channel);                  
         }
     }
 }
